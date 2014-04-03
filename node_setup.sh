15518e8d540c4ac13faffd2b7f1a918a7d4cc68c
Ke Wang <kewang55@gmail.com>
2014-04-03 11:32:51 -0700

    node setup script

---
+++ node_setup.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+sudo apt-get update -y
+sudo apt-get install -y gcc make g++
+
+mkdir ~/Downloads
+cd ~/Downloads
+wget -O node.tar.gz http://nodejs.org/dist/v0.10.26/node-v0.10.26.tar.gz
+tar xvfz node.tar.gz
+cd ~/Downloads/node-v0.10.26/
+./configure
+make
+sudo make install
